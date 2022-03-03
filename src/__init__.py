87bb125601a564d442e0c2c87ee9988549941165
Koowah <anaslargab@gmail.com>
2022-03-03 21:08:18 +0100

    add root dir

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,6 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 BASE_URL = "https://store.steampowered.com/app/{}/"
 REVIEWS_URL = "https://store.steampowered.com/appreviews/{}?\
         json=1&date_range=all&review_type=all&filter=recent&num_per_page=100&cursor={}"
