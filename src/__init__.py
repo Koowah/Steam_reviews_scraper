661cf2bf5df8dc0f5bbe3a8dd7cf3e263bb05cf5
Koowah <anaslargab@gmail.com>
2022-03-21 18:01:59 +0100

    add purchase type all

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,4 @@ import os
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 BASE_URL = "https://store.steampowered.com/app/{}/"
 REVIEWS_URL = "https://store.steampowered.com/appreviews/{}?\
-        json=1&date_range=all&review_type=all&filter=recent&num_per_page=100&cursor={}"
+        json=1&date_range=all&purchase_type=all&filter=recent&num_per_page=100&cursor={}"
